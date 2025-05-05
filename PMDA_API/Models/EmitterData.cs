40b6549d1edc12d548f231726aa15775be09fae1
Amit <amit8289shirke@gmail.com>
2025-05-05 10:52:14 +0530

    Changed in EmitterData model

--- PMDA_API/Models/EmitterData.cs
+++ PMDA_API/Models/EmitterData.cs
@@ -9,6 +9,7 @@
         public string EmitterSymbolCode { get; set; }
         public string EmitterAzimuth { get; set; }
         public string EmitterLethalUnit { get; set; }
+
         public string EmitterRange { get; set; }
         
     }
